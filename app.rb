1cc4fa65377079ba43535c66e8af4bfa8cda1fe6
Michael Roose <mjroose@gmail.com>
2018-10-14 22:17:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Michael"
+  end
+
+  get '/hometown' do
+    "My hometown is Nappanee"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is the Hokey Pokey"
+  end
 end
